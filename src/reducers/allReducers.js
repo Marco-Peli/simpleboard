6e462439a37850f6bcd8c3b3ab1a82d70b54c122
Marco Peli <xxmarco89xx@gmail.com>
2020-09-01 21:39:10 +0200

    - Implemented basic drawing with redux

--- src/reducers/allReducers.js
+++ src/reducers/allReducers.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux';
 import userInputCollectReducer from './userInputCollectReducer';
 import {evaluateUserData, loginRequestReducer} from './signInUpReducers'
 import {handleDrawMenuReducer} from './draw_menu_reducers'
+import {drawAreaReducer} from './draw_area_reducers'
 
 const allReducers = combineReducers({
   userInputCollectReducer,
   loginRequestReducer,
   evaluateUserData,
-  handleDrawMenuReducer
+  handleDrawMenuReducer,
+  drawAreaReducer
 })
 
 export default allReducers;
